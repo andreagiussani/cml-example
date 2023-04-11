fe1765463fdc017e119f048a2bdbe551dac95d0a
andreagiussani <GitHub2019>
2023-04-11 17:57:22 +0200

    :bug: fix train script

--- train.py
+++ train.py
@@ -26,19 +26,11 @@ train_score = regr.score(X_train, y_train) * 100
 # Report test set score
 test_score = regr.score(X_test, y_test) * 100
 
-# Write scores to a file
-with open("outputs/metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
-
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
 # Calculate feature importance in random forest
 importances = regr.feature_importances_
 labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature", "importance"])
 feature_df = feature_df.sort_values(by='importance', ascending=False,)
 
 # image formatting
@@ -47,8 +39,8 @@ title_fs = 22
 sns.set(style="whitegrid")
 
 ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs)
-ax.set_ylabel('Feature', fontsize = axis_fs)
+ax.set_xlabel('Importance',fontsize=axis_fs)
+ax.set_ylabel('Feature', fontsize=axis_fs)
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
 plt.tight_layout()
@@ -56,23 +48,10 @@ plt.savefig("outputs/feature_importance.png", dpi=120)
 plt.close()
 
 
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
+metrics = """
+Accuracy: {:10.4f}
+![Feat Imp](./outputs/feature_importance.png)
+""".format(train_score)
 
-y_pred = regr.predict(X_test) + np.random.normal(0, 0.25, len(y_test))
-y_jitter = y_test + np.random.normal(0, 0.25, len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter, y_pred)), columns=["true", "pred"])
-
-ax = sns.scatterplot(x="true", y="pred", data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality', fontsize = axis_fs)
-ax.set_ylabel('Predicted wine quality', fontsize= axis_fs)
-ax.set_title('Residuals', fontsize=title_fs)
-
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5, 8.5))
-plt.xlim((2.5, 8.5))
-
-plt.tight_layout()
-plt.savefig("outputs/residuals.png", dpi=120)
+with open("metrics.txt", "w") as outfile:
+    outfile.write(metrics)
