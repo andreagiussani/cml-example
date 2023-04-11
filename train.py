bb472f313d3694f1897a4f9e67ccdee927eefc12
andreagiussani <GitHub2019>
2023-04-11 17:42:21 +0200

    :zap: update project

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import numpy as np
 ################################
 
 # Load in the data
-df = pd.read_csv("wine_quality.csv")
+df = pd.read_csv("data/wine_quality.csv")
 
 # Split into train and test sections
 y = df.pop("quality")
@@ -27,7 +27,7 @@ train_score = regr.score(X_train, y_train) * 100
 test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
-with open("metrics.txt", 'w') as outfile:
+with open("outputs/metrics.txt", 'w') as outfile:
         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
@@ -47,12 +47,12 @@ title_fs = 22
 sns.set(style="whitegrid")
 
 ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
+ax.set_xlabel('Importance',fontsize = axis_fs)
 ax.set_ylabel('Feature', fontsize = axis_fs)
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
 plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120) 
+plt.savefig("outputs/feature_importance.png", dpi=120)
 plt.close()
 
 
@@ -60,20 +60,19 @@ plt.close()
 ############ PLOT RESIDUALS  #############
 ##########################################
 
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+y_pred = regr.predict(X_test) + np.random.normal(0, 0.25, len(y_test))
+y_jitter = y_test + np.random.normal(0, 0.25, len(y_test))
+res_df = pd.DataFrame(list(zip(y_jitter, y_pred)), columns=["true", "pred"])
 
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
+ax = sns.scatterplot(x="true", y="pred", data=res_df)
 ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
+ax.set_xlabel('True wine quality', fontsize = axis_fs)
+ax.set_ylabel('Predicted wine quality', fontsize= axis_fs)
+ax.set_title('Residuals', fontsize=title_fs)
 
-# Make it pretty- square aspect ratio
 ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
+plt.ylim((2.5, 8.5))
+plt.xlim((2.5, 8.5))
 
 plt.tight_layout()
-plt.savefig("residuals.png",dpi=120)
+plt.savefig("outputs/residuals.png", dpi=120)
