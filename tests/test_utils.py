572d987cd0ca5d477c506ea508813744704508cb
andreagiussani <GitHub2019>
2023-04-12 14:26:11 +0200

    :zap: improve unittest and script

---
+++ tests/test_utils.py
@@ -0,0 +1,45 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from constants import (
+    FIXED_ACIDITY,
+    SULPHATES_COLNAME,
+    ALCOHOL_COLNAME,
+    PH_COLNAME,
+    QUALITY_COLNAME,
+)
+from tests.fixtures import get_mocked_data
+from utils import (
+    split_train_test,
+    get_feature_importance,
+)
+
+
+class WineQualityUtilsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.columns = [FIXED_ACIDITY, PH_COLNAME, SULPHATES_COLNAME, ALCOHOL_COLNAME, QUALITY_COLNAME]
+        self.raw_data = pd.DataFrame(get_mocked_data(), columns=self.columns)
+
+        self.feature_importance = np.array([0.05743443, 0.03615136, 0.13846394, 0.251952])
+
+    def test__load_dataframe(self):
+        df = self.raw_data
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.shape[0], 10)
+        self.assertEqual(df.shape[1], 5)
+        self.assertListEqual(list(df), self.columns)
+
+    def test__split_train_test(self):
+        X_train, X_test, y_train, y_test = split_train_test(self.raw_data)
+        self.assertIsInstance(X_train, pd.DataFrame)
+        self.assertEqual(X_train.shape[0], 8)
+        self.assertEqual(X_test.shape[0], 2)
+        self.assertEqual(X_train.shape[1], 4)
+        self.assertListEqual(list(X_train), [FIXED_ACIDITY, PH_COLNAME, SULPHATES_COLNAME, ALCOHOL_COLNAME])
+        self.assertEqual(set(y_train.unique().tolist()), set([0, 1]))
+
+    def test__get_feature_importance(self):
+        feature_importance_df = get_feature_importance(self.feature_importance, self.raw_data)
+        self.assertEqual(feature_importance_df.shape[0], 4)
--- train.py
+++ train.py
@@ -1,55 +1,22 @@
-import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-import matplotlib.pyplot as plt
-import seaborn as sns
+import os
+import pandas as pd
 
-import logging
+from sklearn.ensemble import RandomForestClassifier
 
-logger = logging.getLogger('App Logger')
+from constants import FILEPATH, FILENAME
+from utils import split_train_test, get_feature_importance
 
 # Load in the data
-df = pd.read_csv("data/wine_quality.csv")
+df = pd.read_csv(os.path.join(FILEPATH, FILENAME))
 
 # Split into train and test sections
-y = df.pop("quality")
-X_train, X_test, y_train, y_test = train_test_split(
-    df, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = split_train_test(df)
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=2, random_state=42)
-regr.fit(X_train, y_train)
-
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
-
+# Improve using a wider grid of hyperparams - just an example here.
+clf = RandomForestClassifier(max_depth=5, random_state=42)
+clf.fit(X_train, y_train)
+importances = clf.feature_importances_
 
 # Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(
-    list(zip(labels, importances)), columns=["feature", "importance"]
-)
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
-
-# image formatting
-axis_fs = 18
-title_fs = 22
-sns.set(style="whitegrid")
-
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize=axis_fs)
-ax.set_ylabel('Feature', fontsize=axis_fs)
-ax.set_title('Random forest\nfeature importance', fontsize=title_fs)
-
-plt.tight_layout()
-plt.savefig("./outputs/feature_importance.png", dpi=120)
-plt.close()
-
-
-metrics = """train score: {:10.4f}""".format(train_score)
-
-logger.info("Training Finished")
+feature_df = get_feature_importance(importances, df)
